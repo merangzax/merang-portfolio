1863cbf46eb611de5534a19440ef5a5670606454
merangzax <merangzax@gmail.com>
2026-02-06 01:17:42 +0800

    1. chore(root): npm install react-router-dom - create seperate page for gallery and preview - clean up App.js to use react-router-dom for routing
    2. feat(preview.js): added basic layout for preview page
    - title,image
    - next and prev buttons
    - back buttons
    
    3. feat(gallery.js): change artwork gallery title from h1 to h2
    - reason smaller font better ui
    - added 404 error image not found

--- src/App.js
+++ src/App.js
@@ -1,122 +1,17 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import { Box, Typography, ImageList, ImageListItem, useTheme, useMediaQuery } from '@mui/material';
-const importAll = (r) => r.keys().map(r);
-const images = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/));
-
-
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import Gallery from "./components/Gallery"; // your current gallery component
+import Preview from "./components/Preview"; // create this page
 
 function App() {
 
-const [hoveredIndex, setHoveredIndex] = useState(null);
-const [itemData, setItemData] = useState([]);
-
-useEffect(() => {
-  const data = images.map((img, index) => ({
-    id: index,
-    img,
-    title: `Image ${index + 1}`,
-    cols: Math.floor(Math.random() * 2) + 1,
-    rows: Math.floor(Math.random() * 2) + 1,
-  }));
-
-  // shuffle once
-  for (let i = data.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [data[i], data[j]] = [data[j], data[i]];
-  }
-
-  setItemData(data);
-}, []); // ← runs ONCE
-
-const theme = useTheme();
-const sm = useMediaQuery(theme.breakpoints.up('sm'));   // ≥600px
-const md = useMediaQuery(theme.breakpoints.up('md'));   // ≥900px
-const lg = useMediaQuery(theme.breakpoints.up('lg'));   // ≥1200px
-
-let cols = 2;
-if (sm) cols = 4;
-if (md) cols = 6;
-if (lg) cols = 8;
-
-let rowHeight = 200;
-if (sm) rowHeight = 200;
-if (md) rowHeight = 200;
-if (lg) rowHeight = 250;
-  
-  return (
-   <div className='black-page'>
-
-   <Typography
-   variant='h1'
-   fontWeight="bold"
-   align='center'
-   mb={2}
-   >
-  [ARTWORK GALLERY]
-  </Typography>
-
-  <Box display="flex" justifyContent="center">
-  <ImageList
-      sx={{ width: "90vw", height: "100wh", overflow: 'visible',  }}
-      variant="quilted"
-      cols={cols}
-      rowHeight={rowHeight}
-      
-    >
-      {itemData.map((item, index) => {
-      
-      const isHovered = hoveredIndex === index;
-      const isDimmed = hoveredIndex !== null && !isHovered;
-
-      return (
-     
-        <ImageListItem 
-        key={item.img} 
-        cols={item.cols || 1} 
-        rows={item.rows || 1}
-
-      onMouseEnter={() => setHoveredIndex(index)}
-      onMouseLeave={() => setHoveredIndex(null)}
-
-      sx={{
-        borderRadius: 3,
-        overflow: "hidden",
-        transition: "transform 0.35s ease, box-shadow 0.35s ease, opacity 0.35s ease",
-        transform: isHovered ? "scale(1.02)" : isDimmed ? "scale(0.93)" : "scale(0.96)",
-        opacity: isDimmed ? 0.7 : 1,
-        boxShadow: isHovered ? "0 0 30px rgba(255, 255, 255, 0.93)" : "none",
-        zIndex: isHovered ? 10 : 1,
-      }}
-      >
-
-      <Box
-        sx={{
-          width: "100%",
-          height: "100%",
-        }}
-      >
-
-          <img
-            src={item.img}
-            alt={item.title}
-            loading="lazy"
-            style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-            transition: "transform 0.35s ease",
-          }}
-          />
-          </Box>
-        </ImageListItem>
-
-      )
-})}
-    </ImageList>
-    </Box>
-   </div>
-
+ return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/gallery" replace />} />   {/*Temporary redirect*/}
+        <Route path="/gallery" element={<Gallery/>} />
+        <Route path="/preview/:title" element={<Preview />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Preview.js
@@ -0,0 +1,163 @@
+import {useState, useEffect} from "react";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { Box, Typography, Button } from "@mui/material";
+
+
+function Preview() {
+
+  const {title} = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [currentIndex, setCurrentIndex] = useState(null);
+
+  const importAll = (r) => r.keys().map(r);
+  const images = importAll(require.context("../img", false, /\.(png|jpe?g|svg)$/));
+  const itemDataNew = images.map((img, index) => ({
+  id: index,
+  img,
+  title: img.split("/").pop().split(".")[0],
+}));
+
+  const itemData = location.state?.itemData || itemDataNew // get itemData from state
+
+  useEffect(() => {
+    const idx = itemData.findIndex(item => item.title === title);
+    setCurrentIndex(idx !== -1 ? idx : null)
+  }, [title]);
+
+
+  if (currentIndex === null) {
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "grey",
+      }}
+    >
+      <Typography variant="h5">[404 Image Not Found]</Typography>
+    </Box>
+  );
+}
+
+  const imageItem = itemData[currentIndex];
+
+ const goPrev = () => {
+  const nextIndex = currentIndex === 0 ? itemData.length - 1 : currentIndex - 1;
+  setCurrentIndex(nextIndex);
+  navigate(`/preview/${itemData[nextIndex].title}`, { replace: true, state: { itemData }, });
+};
+
+const goNext = () => {
+  const nextIndex = currentIndex === itemData.length - 1 ? 0 : currentIndex + 1;
+  setCurrentIndex(nextIndex);
+  navigate(`/preview/${itemData[nextIndex].title}`, { replace: true, state: { itemData }, });
+};
+
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: "black",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+        textAlign: "center",
+        padding: 2,
+        position:"relative"
+      }}
+    >
+      {/* Small text at top */}
+      <Typography variant="body1" sx={{ mb: 4 }}>
+        {imageItem.title}
+      </Typography>
+
+
+
+      {/* Big image */}
+      <Box
+        component="img"
+        src={imageItem.img}
+        alt={imageItem.title}
+        sx={{
+          maxWidth: "90vw",
+          maxHeight: "80vh",
+          objectFit: "contain",
+          borderRadius: 2,
+          
+        }}
+      />
+
+      {/* Left button */}
+ 
+      <Typography
+        onClick={goPrev}
+        variant='h4'
+        align='center'
+        mb={2}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: 50,
+          transform: "translateY(-50%)",
+          color: "white",
+          cursor:"pointer"
+        }}
+        >
+        {"<"}
+        </Typography>  
+
+
+      {/* Right button */}
+      <Typography
+        onClick={goNext}
+        variant='h4'
+        align='center'
+        mb={2}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          right: 50,
+          transform: "translateY(-50%)",
+          color: "white",
+          cursor:"pointer"
+        }}
+        >
+        {">"}
+        </Typography>  
+
+
+      {/* Back button */}
+      <Button
+        variant="contained"
+        sx={{ 
+          position: "fixed",     
+          top: 16,              
+          left: 35,
+          zIndex: 999,
+          mt: 4, 
+          backgroundColor:"black", 
+          color:"white", 
+          borderRadius:0,
+          cursor:"pointer"
+         }}
+        onClick={() => navigate(-1)}
+      >
+        <Typography
+   variant='h6'
+   align='center'
+   mb={2}
+   >
+  {"< Back"}
+  </Typography>
+      </Button>
+    </Box>
+  );
+}
+
+export default Preview;
